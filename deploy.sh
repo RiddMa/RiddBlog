df6c594720e349a0cc0fb32bc4eb25f7a69c9d30
RiddMa <riddma@qq.com>
2022-01-24 22:49:50 +0800

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ git fetch --all &&
 git reset --hard origin/master &&
 git pull &&
 npm install &&
-npm run build &&
+npm run docs:build &&
 rm -rf /usr/share/nginx/blog/* &&
 cp -r ./dist/* /usr/share/nginx/blog &&
 nginx -s reload &&
